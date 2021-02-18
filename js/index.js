d846c9a2319ebb0bc7ec80de7b8e8d2efba759cb
QYK <13526670136@163.com>
2021-02-18 16:54:09 +0800

    zuixin

--- js/index.js
+++ js/index.js
@@ -11,4 +11,6 @@ var myName = 'QYK';
 var myAge = 18;
 var sentence = "Hello, my name is " + myName + ", I'll be " + (myAge + 1) + " years old next month";
 var helloTypeScript = new HelloTypeScript(sentence);
-document.body.innerHTML = helloTypeScript.hello();
+// document.body.innerHTML = helloTypeScript.hello();
+var test = '运行测试';
+var test1 = '运行测试1';
--- ts/index.ts
+++ ts/index.ts
@@ -12,4 +12,7 @@ let myName: string = 'QYK';
 let myAge: number = 18;
 let sentence: string = `Hello, my name is ${myName}, I'll be ${myAge + 1} years old next month`
 let helloTypeScript: HelloTypeScript = new HelloTypeScript(sentence);
-document.body.innerHTML = helloTypeScript.hello();
+// document.body.innerHTML = helloTypeScript.hello();
+
+let test:string = '运行测试'
+let test1:string = '运行测试1'
